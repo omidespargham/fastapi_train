e7048ec5f0070cda8b60655f6285a7c42046f26c
omidespargham <omidespargham2001@gmail.com>
2023-02-15 12:56:43 +0330

    first migration handeled and we create the user !

--- migrations/env.py
+++ migrations/env.py
@@ -20,7 +20,7 @@ if config.config_file_name is not None:
 # target_metadata = mymodel.Base.metadata
 
 from models import Base
-target_metadata = Base
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
