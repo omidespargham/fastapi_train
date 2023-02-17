5918c8118a5ae5ce28674135b1fdbfefa0877923
omidespargham <omidespargham2001@gmail.com>
2023-02-17 13:04:02 +0330

    we worked with Query Meta data and ...

--- routers/user/user_post.py
+++ routers/user/user_post.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter,Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Query,Body
 from sqlalchemy.orm import Session
 from database import SessionLocal, engine
 import schema
@@ -6,7 +6,8 @@ import models
 import database
 # from two import get_db
 
-router = APIRouter(prefix="",tags=["user_post"])
+router = APIRouter(prefix="", tags=["user_post"])
+
 
 def get_db():
     db = SessionLocal()
@@ -15,25 +16,37 @@ def get_db():
     finally:
         db.close()
 
-@router.post("/create_post/",response_model=schema.PostShow,summary="Sakhte post !"
-             ,description="in api baraye shoma post misazad"
-             ,response_description="dar javab b shoma etelaate post ra br migardanad")
-def create_post(post:schema.PostCreate,db:Session=Depends(get_db)):
-    post = models.Post(title=post.title,description=post.description)
+
+@router.post("/create_post/", response_model=schema.PostShow, summary="Sakhte post !", description="in api baraye shoma post misazad", response_description="dar javab b shoma etelaate post ra br migardanad")
+def create_post(post: schema.PostCreate, db: Session = Depends(get_db)):
+    post = models.Post(title=post.title, description=post.description)
     db.add(post)
     db.commit()
     db.refresh(post)
     return post
 
 
-@router.post("/create_user/",response_model=schema.UserShow,summary="sakhte user"
-             ,description="is api baraye shoma post misazad")
-def create_user(user:schema.UserCreate,db:Session= Depends(get_db)):
-    db_user = db.query(models.User).filter(models.User.email==user.email).first()
+@router.post("/create_user/", response_model=schema.UserShow, summary="sakhte user", description="is api baraye shoma post misazad")
+def create_user(user: schema.UserCreate, db: Session = Depends(get_db)):
+    db_user = db.query(models.User).filter(
+        models.User.email == user.email).first()
     if db_user:
-        raise HTTPException(status_code=400,detail="emial already exist !")
-    user = models.User(email=user.email,username=user.username,password=user.password)
+        raise HTTPException(status_code=400, detail="emial already exist !")
+    user = models.User(
+        email=user.email, username=user.username, password=user.password)
     db.add(user)
     db.commit()
     db.refresh(user)
+    return user
+
+
+@router.post("/test_user/", response_model=schema.UserShow)
+def test_user(user: schema.UserCreate,
+              name: str = Query(Ellipsis,
+                                title="this is the title",
+                                description="this is description",
+                                alias="username",
+                                deprecated=False),
+              content: str = Body(Ellipsis, # required 
+                                title="this is content")):
     return user
--- schema.py
+++ schema.py
@@ -1,5 +1,7 @@
 from pydantic import BaseModel
 from models import Post
+from typing import Optional
+from fastapi import Body
 
 # User schema
 
@@ -9,15 +11,20 @@ class UserBase(BaseModel):
 
 
 class UserCreate(UserBase):
-    password: str
+    password: str = Body(Ellipsis,
+                         min_length=8)
 
 
 class UserShow(UserBase):
-    id: int
+    id: Optional[int]
     # posts: list[Post] = []
 
     class Config:
-        orm_mode = True # if i send you obj of my db you tern if to json data
+        orm_mode = True # if i send you obj of my db, schema can tern it to json data
+                          # if it is True. without schema db obj will return auto to json.
+                          # be carefull schema objects can return to json automatically
+                          # this orm_mode just use when we want to give db obj to schema class
+                          # for make it as json
 
 ################################
 
